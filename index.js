c46d9b68ba0f3f70995a254703851bd520e0ec55
Ariel Malka <93776832+malkaari@users.noreply.github.com>
2023-07-18 12:54:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
@@ -27,6 +29,11 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
